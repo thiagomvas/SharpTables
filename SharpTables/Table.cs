073d6f2306914d4e2ec5898e83864806d3a19119
Thiago Menezes <thiagomvas@gmail.com>
2024-06-27 12:23:31 -0300

    Add ToString overrides

--- SharpTables/Table.cs
+++ SharpTables/Table.cs
@@ -1,4 +1,6 @@
-﻿namespace SharpTables
+﻿using System.Text;
+
+namespace SharpTables
 {
 	public class Table
 	{
@@ -134,6 +136,84 @@
 			PrintHorizontalDivider(widestCellPerColumn, Formatting.BottomLeftDivider, Formatting.BottomMiddleDivider, Formatting.BottomRightDivider, Formatting.HorizontalDivider, Formatting.DividerColor);
 		}
 
+		public override string ToString()
+		{
+			return ToString(false);
+		}
+		public string ToString(bool separateHeader)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			// Setup
+			int[] widestCellPerColumn = new int[Rows.Max(r => r.Cells.Count)];
+			int columnCount = Rows[0].Cells.Count;
+			foreach (Row row in Rows)
+			{
+				for (int i = 0; i < row.Cells.Count; i++)
+				{
+					var cell = row.Cells[i];
+					int cellWidth = Utils.MeasureStringWidth(cell.Text) + cell.PaddingRight;
+					if (cellWidth > widestCellPerColumn[i])
+					{
+						widestCellPerColumn[i] = cellWidth;
+					}
+				}
+			}
+
+			// Print the header dividers
+			if (Formatting.Header.HasTopDivider)
+				sb.Append(HorizontalDivider(widestCellPerColumn, Formatting.Header.TopLeftDivider, Formatting.Header.TopMiddleDivider, Formatting.Header.TopRightDivider, Formatting.Header.HorizontalDivider, Formatting.Header.DividerColor));
+
+			// Print the header row
+			Row headerRow = Rows.First();
+			foreach (var cell in headerRow.Cells)
+			{
+				sb.Append($"{Formatting.Header.VerticalDivider}");
+				sb.Append($"{Utils.ResizeStringToWidth(cell.Text, widestCellPerColumn[headerRow.Cells.IndexOf(cell)])}");
+			}
+			sb.AppendLine($"{Formatting.Header.VerticalDivider}");
+
+			if (separateHeader)
+			{
+				sb.Append(HorizontalDivider(widestCellPerColumn, Formatting.Header.BottomLeftDivider, Formatting.Header.BottomMiddleDivider, Formatting.Header.BottomRightDivider, Formatting.Header.HorizontalDivider, Formatting.Header.DividerColor));
+				sb.Append(HorizontalDivider(widestCellPerColumn, Formatting.TopLeftDivider, Formatting.TopMiddleDivider, Formatting.TopRightDivider, Formatting.HorizontalDivider, Formatting.DividerColor));
+			}
+			else
+			{
+				sb.Append(HorizontalDivider(widestCellPerColumn, Formatting.Header.LeftMiddleDivider, Formatting.Header.MiddleDivider, Formatting.Header.RightMiddleDivider, Formatting.Header.HorizontalDivider, Formatting.Header.DividerColor));
+			}
+
+			foreach (Row row in Rows.Skip(1))
+			{
+				// Print the row with cell values
+				for (int i = 0; i < columnCount; i++)
+				{
+					// If the row has fewer cells than the header, add empty cells
+					if (i >= row.Cells.Count)
+					{
+						row.Cells.Add(new Cell(""));
+					}
+
+					Cell cell = row.Cells[i];
+					string cellText = Utils.ResizeStringToWidth(cell.Text, widestCellPerColumn[i], cell.IsNumeric ? NumberAlignment : TextAlignment);
+					sb.Append($"{Formatting.VerticalDivider}");
+					sb.Append($"{cellText}");
+				}
+				sb.AppendLine($"{Formatting.VerticalDivider}");
+
+				// Print the middle divider
+				if (row != Rows.Last())
+				{
+					sb.Append(HorizontalDivider(widestCellPerColumn, Formatting.LeftMiddleDivider, Formatting.MiddleDivider, Formatting.RightMiddleDivider, Formatting.HorizontalDivider, Formatting.DividerColor));
+				}
+			}
+
+			// Print the bottom divider
+			sb.Append(HorizontalDivider(widestCellPerColumn, Formatting.BottomLeftDivider, Formatting.BottomMiddleDivider, Formatting.BottomRightDivider, Formatting.HorizontalDivider, Formatting.DividerColor));
+
+			return sb.ToString();
+		}
+
 		public static Table FromDataSet(object[,] data) => FromDataSet(data, new Formatting());
 		public static Table FromDataSet(object[,] data, Formatting formatting)
 		{
@@ -149,7 +229,6 @@
 			}
 			return table;
 		}
-
 		public static Table FromDataSet(IEnumerable<IEnumerable<object>> data) => FromDataSet(data, new Formatting());
 		public static Table FromDataSet(IEnumerable<IEnumerable<object>> data, Formatting formatting)
 		{
@@ -176,6 +255,23 @@
 			Console.WriteLine(right);
 			Console.ResetColor();
 		}
-
+		private string HorizontalDivider(int[] widths, char left, char middle, char right, char horizontal, ConsoleColor color)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(left);
+			foreach (int width in widths)
+			{
+				for (int i = 0; i < width; i++)
+				{
+					sb.Append(horizontal);
+				}
+				sb.Append(middle);
+			}
+			sb.Remove(sb.Length - 1, 1); // Remove the last middle
+			sb.Append(right);
+			sb.AppendLine();
+			return sb.ToString();
+		}
 	}
 }
+
