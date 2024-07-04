70d75af0846c379fd17a588419c97bb35886dfee
Thiago Menezes <thiagomvas@gmail.com>
2024-07-04 12:54:03 -0300

    feat: Add TableIgnoreAttribute

---
+++ SharpTables/Annotations/TableIgnoreAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpTables.Annotations
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class TableIgnoreAttribute : Attribute
+    {
+        public TableIgnoreAttribute()
+        {
+        }
+    }
+}
