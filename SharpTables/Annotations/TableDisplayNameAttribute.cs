363da576943970c279a267f56aa79009e8c92cfc
Thiago Menezes <thiagomvas@gmail.com>
2024-07-04 12:53:32 -0300

    feat: Add TableDisplayNameAttribute to set a property display name on table.

---
+++ SharpTables/Annotations/TableDisplayNameAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpTables.Annotations
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class TableDisplayNameAttribute : Attribute
+    {
+        readonly string _name;
+
+        // This is a positional argument
+        public TableDisplayNameAttribute(string displayName)
+        {
+            _name = displayName;
+
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
