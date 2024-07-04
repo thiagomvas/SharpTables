02e18fb8e6600107a94020f5924108e2a8cf4c94
Thiago Menezes <thiagomvas@gmail.com>
2024-07-04 12:53:54 -0300

    feat: Add TableColorAttribute

---
+++ SharpTables/Annotations/TableColorAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpTables.Annotations
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class TableColorAttribute : Attribute
+    {
+        readonly ConsoleColor _color;
+
+        // This is a positional argument
+        public TableColorAttribute(ConsoleColor color)
+        {
+            _color = color;
+
+        }
+
+        public ConsoleColor Color
+        {
+            get { return _color; }
+        }
+    }
+}
