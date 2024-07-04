b50309488321251b76e4ff78d9535b06bc5119c8
Thiago Menezes <thiagomvas@gmail.com>
2024-07-04 14:02:26 -0300

    feat: Migrate Table settings to its own class

---
+++ SharpTables/TableSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpTables
+{
+    public class TableSettings
+    {
+        public string NullOrEmptyReplacement { get; set; } = "Null";
+        public Action<Cell> CellPreset { get; set; } = _ => { };
+        public Formatting TableFormatting { get; set; } = Formatting.Default;
+        public bool UseHeader { get; set; } = true;
+        public bool DisplayRowCount { get; set; } = false;
+        public bool DisplayRowIndexes { get; set; } = false;
+        public ConsoleColor RowIndexColor { get; set; } = ConsoleColor.DarkGray;
+
+    }
+}
