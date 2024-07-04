c4dd839dad8f24f19d742249c8014ec0db9fca14
Thiago Menezes <thiagomvas@gmail.com>
2024-07-04 12:53:43 -0300

    feat: Add TableAlignmentAttribute

---
+++ SharpTables/Annotations/TableAlignmentAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpTables.Annotations
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class TableAlignmentAttribute : Attribute
+    {
+        readonly Alignment _alignment;
+
+        // This is a positional argument
+        public TableAlignmentAttribute(Alignment alignment)
+        {
+            _alignment = alignment;
+
+        }
+
+        public Alignment Alignment
+        {
+            get { return _alignment; }
+        }
+    }
+}
